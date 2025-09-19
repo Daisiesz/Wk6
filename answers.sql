02477cd21e566304afa9d3afb6b55cb49a8166cc
Daisiesz <beckybecklyn@gmail.com>
2025-09-19 16:22:22 +0330

    Complete assignment

--- answers.sql
+++ answers.sql
@@ -1,6 +1,20 @@
--- Answer 1: Using Inner Join to get names, emails and codes
-SELECT users.name, users.email, codes.code
-FROM users
-INNER JOIN codes ON users.id = codes.user_id;
+-- Answer 1: Get employee details with office code using INNER JOIN
+USE salesdb;
+SELECT * FROM employees, offices
 
--- Answer 2: Using left join to
+SELECT employees.firstName, employees.lastName, employees.email, employees.officeCode
+FROM employees
+INNER JOIN offices ON employees.officeCode = offices.officeCode;
+
+-- Answer 2: Using left join to get prodct info.
+SELECT products.productName, products.productVendor, products.productLine
+FROM products
+LEFT JOIN productlines
+ON products.productLine =  productlines.productLine;
+
+-- Answer 3: Use right join to get the first 10 order information.
+SELECT orders.orderDate, orders.shippedDate, orders.status, orders.customerNumber
+FROM orders
+RIGHT JOIN customers 
+ON orders.customerNumber = customers.customerNumber
+LIMIT 10;
